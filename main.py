40709a4d0a4b846d963c2d6570b177e3c711599d
Kirill K <kovalev.kirill.a@gmail.com>
2023-05-21 11:30:12 +0300

    refactor: changes

--- main.py
+++ main.py
@@ -2,20 +2,16 @@ import asyncio
 import logging
 import nats
 import os
-import time
 
 from lib.handler import Handler
 
 app_name = os.environ.get("APP_NAME", "personal-assistant")
 
 nats_address = os.environ.get("NATS_ADDR", "nats://nats.nats.svc:4222")
-nats_prefix = os.environ.get("NATS_PREFIX")
-nats_subj_inbox = os.environ.get("NATS_SUBJ_INBOX", "inbox")
-nats_subj_response = os.environ.get("NATS_SUBJ_RESPONSE", "response")
+nats_prefix = os.environ.get("NATS_PREFIX", "dummy")
 
-nats_prefix = nats_prefix or app_name
-nats_subj_in = f"{nats_prefix}.tg.{nats_subj_inbox}.>"
-nats_subj_out = f"{nats_prefix}.tg.{nats_subj_response}"
+nats_subj_in = f"{nats_prefix}.tg.in"
+nats_subj_out = f"{nats_prefix}.tg.out"
 
 
 async def main() -> None:
@@ -23,13 +19,12 @@ async def main() -> None:
     nc = await nats.connect(nats_address)
 
     js = nc.jetstream()
-    await js.add_stream(name=app_name, subjects=[nats_subj_out, f"{nats_subj_out}.*"])
 
     handler = Handler(jetstream=js, nats_subj_response=nats_subj_out)
 
-    logging.warning(f"getting updates for subject: {nats_subj_in}")
+    logging.warning(f"getting updates for subject: {nats_subj_in}.>")
 
-    await js.subscribe(nats_subj_in, "workers", cb=handler.echo)
+    await js.subscribe(f"{nats_subj_in}.>", "workers", cb=handler.echo)
 
     while True:
         await asyncio.sleep(1)
